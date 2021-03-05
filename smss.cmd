70d958b770378db9b4cf35eef3c03c670029c3dc
Spafbi <spafbi@outlook.com>
2021-03-05 15:39:11 -0600

    Add pip and pip module support

--- smss.cmd
+++ smss.cmd
@@ -4,23 +4,58 @@ chcp 65001>nul
 set BASEPATH=%~dp0
 set BASEPATH=%BASEPATH:~0,-1%
 
+if NOT "%BASEPATH%"=="%BASEPATH: =%" goto :noSpaces
+
 set PYTHONURL=https://www.python.org/ftp/python/3.9.2/python-3.9.2-embed-amd64.zip
-set PYTHONDIR=%BASEPATH%/python
-set PYTHONBIN=%PYTHONDIR%/python.exe
-set TEMPDIR=%BASEPATH%/temp
+set PYTHONDIR=%BASEPATH%\python
+set PYTHONBIN=%PYTHONDIR%\python.exe
+set PIPBIN=%PYTHONDIR%\Scripts\pip.exe
+set PIPURL=https://bootstrap.pypa.io/get-pip.py
+set SMSSTEMP=%BASEPATH%\temp
 goto :pythonCheck
 
 :pythonCheck
-if exist "%PYTHONBIN%" goto :runScript
-if not exist "%TEMPDIR%" (
-  md "%TEMPDIR%"
+if exist "%PYTHONBIN%" goto :pipCheck
+echo Downloading and installing embedded Python...
+if not exist "%SMSSTEMP%" (
+  md "%SMSSTEMP%"
 )
-curl -L %PYTHONURL% -o "%TEMPDIR%/python.zip"
-powershell Expand-Archive -LiteralPath '%TEMPDIR%/python.zip' -DestinationPath '%PYTHONDIR%'
+curl -L %PYTHONURL% -o "%SMSSTEMP%/python.zip"
+powershell Expand-Archive -LiteralPath '%SMSSTEMP%/python.zip' -DestinationPath '%PYTHONDIR%'
+setlocal
+>%PYTHONDIR%\python39._pth (
+    for %%I in (
+        "python39.zip"
+        "."
+        ""
+        "import site"
+    ) do echo %%~I
+)
+goto :pipCheck
+
+:pipCheck
+if exist "%PIPBIN%" goto :moduleCheck
+echo Setting up Python pip...
+curl -L %PIPURL% -o "%SMSSTEMP%/get-pip.py"
+"%PYTHONBIN%" "%SMSSTEMP%/get-pip.py" --no-warn-script-location
+goto :moduleCheck
+
+:moduleCheck
+for %%x in (bs4 requests) do (
+  if not exist %PYTHONDIR%\Lib\site-packages\%%x\__init__.py (
+    echo Installing pip module %%x
+    "%PYTHONBIN%" -m pip install -U %%x --no-warn-script-location
+  )
+)
 goto :runScript
 
 :runScript
 "%PYTHONBIN%" .\smss-core.py
+goto :end
+
+:noSpaces
+echo This script cannot be run from a path having spaces.
+echo     %BASEPATH%
 goto :end
 
 :end
